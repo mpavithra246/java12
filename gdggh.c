621b6efeb5564bbd449931bbc267c602ce82fb37
mpavithra246 <mpavithra246@gmail.com>
2016-08-08 12:39:12 +0530

    gvdgv
    hvdugt

---
+++ gdggh.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int v,x,pair,i;
+printf("enter the child");
+scanf("%d",&v);
+printf("pairs");
+scanf("%d",&pair);
+for(i=0;i<2*v;i++)
+{
+x=2*v-i;
+if(pair!=x)
+{
+printf("The pair is%d for days %d is %d\n",p,j++,p);
+}
+}
+return 0;
+}
